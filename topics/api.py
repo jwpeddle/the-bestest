b8c6e393370895669360e4dc4b8a1c35ec708d5b
Jason Peddle <jwpeddle@gmail.com>
2012-12-01 15:59:04 -0500

    add more endpoints, turn off auth

--- topics/api.py
+++ topics/api.py
@@ -1,9 +1,27 @@
 from tastypie import fields
+from tastypie.authentication import Authentication
+from tastypie.authorization import Authorization
 from tastypie.resources import ModelResource
 
-from topics.models import Topic
+from topics.models import Entry, Topic, Vote
+
+
+class EntryResource(ModelResource):
+    class Meta:
+        queryset = Entry.objects.all()
+        authentication = Authentication()
+        authorization = Authorization()
 
 
 class TopicResource(ModelResource):
     class Meta:
         queryset = Topic.objects.all()
+        authentication = Authentication()
+        authorization = Authorization()
+
+
+class VoteResource(ModelResource):
+    class Meta:
+        queryset = Vote.objects.all()
+        authentication = Authentication()
+        authorization = Authorization()
