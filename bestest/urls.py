bac338639dca599935f4301c54282ee58a7f587c
Jason Peddle <jwpeddle@gmail.com>
2012-12-01 14:11:33 -0500

    send all urls to base.html

--- bestest/urls.py
+++ bestest/urls.py
@@ -5,6 +5,6 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    url(r'^$', TemplateView.as_view(template_name='base.html'), name='home'),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'', TemplateView.as_view(template_name='base.html'), name='home'),
 )
