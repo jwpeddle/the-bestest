d1de6185af44fadb6d7b9b1007788cc5a7dc4b9e
Jason Peddle <jwpeddle@gmail.com>
2012-12-01 15:38:52 -0500

    add topic resource

--- bestest/urls.py
+++ bestest/urls.py
@@ -1,10 +1,17 @@
 from django.conf.urls import patterns, include, url
-from django.views.generic.base import TemplateView
-
 from django.contrib import admin
+from django.views.generic.base import TemplateView
+from tastypie.api import Api
+
+from topics.api import TopicResource
+
 admin.autodiscover()
+
+api = Api(api_name='v1')
+api.register(TopicResource())
 
 urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^api/', include(api.urls)),
     url(r'', TemplateView.as_view(template_name='base.html'), name='home'),
 )
