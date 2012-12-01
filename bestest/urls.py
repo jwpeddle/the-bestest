83b34de1b8d15be3fd0dee17d43dae4bf046fc72
Jason Peddle <jwpeddle@gmail.com>
2012-12-01 18:23:46 -0500

    homepage redirect

--- bestest/urls.py
+++ bestest/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
-from django.views.generic.base import TemplateView
+from django.views.generic import RedirectView, TemplateView
 from tastypie.api import Api
 
 from topics.api import EntryResource, TopicResource, VoteResource
@@ -15,5 +15,6 @@ api.register(VoteResource())
 urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^api/', include(api.urls)),
+    url(r'^$', RedirectView.as_view(url='/1')),
     url(r'', TemplateView.as_view(template_name='base.html'), name='home'),
 )
