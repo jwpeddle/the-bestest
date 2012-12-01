ba954075793a40d1a5d4f3aa45e98da01f446956
Diego Muñoz Escalante <escalant3@gmail.com>
2012-12-01 18:13:16 -0500

    Pagination

--- site_static/js/controllers/topic.js
+++ site_static/js/controllers/topic.js
@@ -2,10 +2,17 @@ App.controller('TopicCtrl', [
   '$scope',
   '$routeParams',
   'models',
-  function($scope, $routeParams, models) {
+  'pagination',
+  function($scope, $routeParams, models, pagination) {
+
+    $scope.pagination = pagination;
 
     $scope.topic = models.Topic.get({topicId: $routeParams.topic_id}, function(response) {
       $scope.topic = response;
+      pagination.setItems(_.sortBy(response.entries,
+                                    function(n) {
+                                      return -n.number_of_votes;
+                                    }));
     });
 
     $scope.addNewEntry = function(entry) {
---
+++ site_static/js/factories/pagination.js
@@ -0,0 +1,92 @@
+App.factory('pagination', [
+  function() {
+    var currentPage = 0;
+    var maxPages = 0;
+    var elementsPerPage = 10;
+    var items = [];
+
+    function setPage(page) {
+      if (page >= 0 && page < maxPages) {
+        currentPage = page;
+      }
+    }
+
+    function setMaxPages(numberOfItems) {
+        maxPages = Math.max(Math.ceil(numberOfItems / elementsPerPage), 1);
+    }
+
+
+    function setItems(_items) {
+      if (!!_items) {
+        items = _items;
+        setMaxPages(items.length);
+      }
+    }
+
+    function nextPage() {
+      setPage(currentPage + 1);
+    }
+
+    function previousPage() {
+      setPage(currentPage - 1);
+    }
+
+    function getSelection() {
+      var start, end, selection;
+
+      start = currentPage * elementsPerPage;
+      end = (currentPage + 1) * elementsPerPage;
+
+      selection = items.slice(start, end);
+      if (!selection.length && !isFirst()) {
+        currentPage = 0;
+        return getSelection();
+      }
+
+      return selection;
+    }
+
+    function isFirst() {
+      return currentPage === 0;
+    }
+
+    function isLast() {
+      return ((currentPage+1) === maxPages);
+    }
+
+    function isEmpty() {
+      return !items.length;
+    }
+
+    function getCurrentPage() {
+      return currentPage;
+    }
+
+    function getMaxPage() {
+      return maxPages;
+    }
+
+    function setElementsPerPage(elements) {
+      elementsPerPage = elements;
+      setMaxPages(items);
+    }
+
+    function getElementsPerPage() {
+      return elementsPerPage;
+    }
+
+    return {
+      nextPage: nextPage,
+      previousPage: previousPage,
+      isFirst: isFirst,
+      isLast: isLast,
+      isEmpty: isEmpty,
+      getSelection: getSelection,
+      getCurrentPage: getCurrentPage,
+      getMaxPage: getMaxPage,
+      setItems: setItems,
+      getElementsPerPage: getElementsPerPage,
+      setElementsPerPage: setElementsPerPage
+    };
+  }
+]);
