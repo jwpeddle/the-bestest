fda8fe398c4e08ae81fb46a51ad3fffdac0fcd62
Gregory Pike <greg@gregpike.ca>
2012-12-01 14:56:30 -0500

    Added our stylesheet

--- site_static/js/dfp-ad-tags.js
+++ site_static/js/dfp-ad-tags.js
@@ -10,9 +10,7 @@ gads.src = (useSSL ? 'https:' : 'http:') +
 var node = document.getElementsByTagName('script')[0];
 node.parentNode.insertBefore(gads, node);
 })();
-</script>
 
-<script type='text/javascript'>
 googletag.cmd.push(function() {
 googletag.defineSlot('/1041600/The_Best', [300, 250], 'div-gpt-ad-1354389945924-0').addService(googletag.pubads());
 googletag.pubads().enableSingleRequest();
