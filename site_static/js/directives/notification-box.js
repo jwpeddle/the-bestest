023634ff3779d272b0778e6bcb018824d483789a
Diego Muñoz Escalante <escalant3@gmail.com>
2012-12-01 20:46:31 -0500

    Notifications on voting and entry creation

--- site_static/js/directives/notification-box.js
+++ site_static/js/directives/notification-box.js
@@ -7,17 +7,23 @@ App.directive('notificationBox', [
       templateUrl: '/static/templates/notification-box.html',
       link: function(scope, elem, attrs) {
 
-        scope.$watch('visible', function() {
-          $timeout(function() {
-            scope.visible = false;
-          }, 5000);
+        scope.$watch('visible', function(value) {
+          if (!!value) {
+            $timeout(function() {
+              scope.visible = false;
+            }, 5000);
+          }
         });
 
         // Applicaton messages
-        scope.$on("bestest.models.entry.created", function(entry) {
-          scope.text = "Entry " + entry.name + " succesfully created!";
+        scope.$on("bestest.models.entry.willCreate", function(m, entryName) {
+          scope.text = "Entry " + entryName + " succesfully created!";
           scope.visible = true;
-          scope.$apply();
+        });
+
+        scope.$on("bestest.models.vote.willCreate", function(m) {
+          scope.text = "Your vote has been registered!";
+          scope.visible = true;
         });
       }
     };
