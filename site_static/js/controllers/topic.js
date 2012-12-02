4348f207264037a35924e864f3263d0fae4f4865
Diego Muñoz Escalante <escalant3@gmail.com>
2012-12-01 22:33:59 -0500

    Improvements showing last voted date

--- site_static/js/controllers/topic.js
+++ site_static/js/controllers/topic.js
@@ -3,21 +3,21 @@ App.controller('TopicCtrl', [
   '$routeParams',
   'models',
   'pagination',
-  function($scope, $routeParams, models, pagination) {
+  '$timeout',
+  'VOTES_REFRESH_INTERVAL',
+  function($scope, $routeParams, models, pagination, $timeout,
+          VOTES_REFRESH_INTERVAL) {
 
     $scope.pagination = pagination;
 
-    $scope.topic = models.Topic.get({topicId: $routeParams.topic_id}, function(response) {
-      $scope.topic = response;
-      refreshEntries(response.entries);
-    });
+    $scope.topic = getTopicInfo();
 
     $scope.addNewEntry = function(entry) {
       models.Entry.create($routeParams.topic_id, entry);
     };
 
     $scope.addVote = function(entry) {
-      entry.number_of_votes++;
+      refreshVotes(entry);
       models.Vote.create(entry.id);
     };
 
@@ -32,5 +32,26 @@ App.controller('TopicCtrl', [
                                 return -n.number_of_votes;
                               }));
     }
+
+    // Introduce a fake vote while the entry refreshes
+    function refreshVotes(entry) {
+      entry.number_of_votes++;
+      entry.votes.push({
+        date: moment().format("YYYY-MM-DD")
+      });
+    }
+
+    function getTopicInfo() {
+      return  models.Topic.get({topicId: $routeParams.topic_id}, function(response) {
+        $scope.topic = response;
+        refreshEntries(response.entries);
+      });
+    }
+
+    setInterval(function() {
+      console.log("RR");
+      getTopicInfo();
+    }, VOTES_REFRESH_INTERVAL);
+
   }
 ]);
