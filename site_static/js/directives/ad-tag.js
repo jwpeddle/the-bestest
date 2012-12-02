00fc1a99d1a3290374839bc386bada808d76d265
Gregory Pike <greg@gregpike.ca>
2012-12-01 21:10:02 -0500

    Ads - exclaimation mark

--- site_static/js/directives/ad-tag.js
+++ site_static/js/directives/ad-tag.js
@@ -33,11 +33,31 @@ App.directive('adTag', function() {
       // Typically these ads would be loaded directly from DFP
       var ads = [
         {ad:'/static/ads/coffee.jpg', tags:['coffee'], ad_unit:'The_Best'},
-        {ad:'/static/ads/coffee.jpg', tags:['coffee'], ad_unit:'The_Best'}
+        {ad:'/static/ads/ambassador-ad.png', tags:[], ad_unit:'The_Best'},
+        {ad:'/static/ads/chango-ad.png', tags:[], ad_unit:'The_Best'},
+        {ad:'/static/ads/extreme-startup-ad.png', tags:[], ad_unit:'The_Best'},
+        {ad:'/static/ads/microsoft-ad.png', tags:[], ad_unit:'The_Best'},
+        {ad:'/static/ads/vm-farms.png', tags:[], ad_unit:'The_Best'}
       ];
+      // Filter by ad unit
       ads = _.filter(ads, function(ad) { return ad.ad_unit==='The_Best' });
-      var randomAd = _.random(_.size(ads));
-      scope.displayAd = ads[0].ad; 
+      // Filter by tag
+      ads = _.filter(ads, function(ad) {
+                // If this ad has no tags to target we return true and the ad is good to show
+                if (!!attrs.tag) {
+                  // If this ad requires a tag and there no tag for this unit we do not want to show it.
+                  if (_.size(ad.tags)<1) return false;
+                  // Let's assume we do not show this ad unless we find an ad tag that matches the tag specified for this unit
+                  var validTag = false;
+                  _.each(ad.tags,function(tag){
+                    if (tag===attrs.tag) validTag = true;
+                  });
+                  return validTag;
+                }
+                return true;    
+              });
+      var randomAd = _.random(_.size(ads)-1);
+      scope.displayAd = ads[randomAd].ad; 
       // End of silly hack
 
       // googletag.cmd.push(function() {
