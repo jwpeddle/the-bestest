50b63706688a6a1a0dc9f515de8519c45ee6d3d9
Jason Peddle <jwpeddle@gmail.com>
2012-12-01 23:44:12 -0500

    bettererer vote data

--- topics/autofixtures.py
+++ topics/autofixtures.py
@@ -6,7 +6,7 @@ from topics.models import Vote
 
 class DateTimeGenerator(generators.DateTimeGenerator):
     min_date = datetime.datetime(2011, 1, 1, 0, 0)
-    max_date = datetime.datetime(2013, 1, 1, 0, 0)
+    max_date = datetime.datetime(2012, 12, 1, 0, 0)
 
 
 class VoteAutoFixture(AutoFixture):
